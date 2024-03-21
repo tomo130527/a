5c927064d11aa8a9093a8592c5b556a69b810b2e
Naresh Dhungana <hinareshg@gmail.com>
2024-03-21 18:09:28 +0900

    uu

--- samppyyy.py
+++ samppyyy.py
@@ -1,11 +1,8 @@
-# Import SymPy
 from sympy import symbols, Eq, solve
 
-# Define symbols
+# Define symbols and equation
 x, y = symbols('x y')
-
-# Define an equation
-equation = Eq(x**2 + y**2, 25)
+equation = x**2 + y**2 - 25
 
 # Solve the equation for x
 solution_x = solve(equation, x)
@@ -13,9 +10,8 @@ solution_x = solve(equation, x)
 # Display the solution for x
 print("Solution for x:", solution_x)
 
-# Substitute a specific value for y and solve for x
-equation_substituted = equation.subs(y, 3)
-solution_substituted = solve(equation_substituted, x)
+# Substitute a specific value for if y=3 and solve for x
+solution_substituted = solve(equation.subs(y, 3), x)
 
 # Display the substituted solution for x
 print("Substituted solution for x:", solution_substituted)
