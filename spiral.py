3e11282d8fe5c3ef6ce945604c27ae37973d344f
Naresh Dhungana <hinareshg@gmail.com>
2024-06-05 14:10:52 +0900

    finfal y axis maintanifn

--- spiral.py
+++ spiral.py
@@ -6,6 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 
 radius_of_coil=2.5 
+total_steps = 300
 
 def save_to_csv(data, filename):
     with open(filename, 'w', newline='') as csvfile:
@@ -40,11 +41,11 @@ def plot_csv_file(filename):
     ax.set_zlim([0, 30])  # Example z-axis range
     plt.show()
 
-def spiral(step_size=0.01):
+def spiral():
     x_data = []
     y_data = []
     z_data = []
-    total_steps = 20
+    
     base_height = 1
     segment2 = base_height + 1.5
     total_height = segment2 + 2.5
@@ -66,16 +67,15 @@ def spiral(step_size=0.01):
         x_data.append(x)
         y_data.append(y)
         z_data.append(z)
-        iin += step_size
+        iin += 1
 
     return list(zip(x_data, y_data, z_data))
 
 
-def l_shaped_x_axis(step_size=0.01):
+def l_shaped_x_axis():
     x_data = []
     y_data = []
     z_data = []
-    total_steps = 20
     base_height = 1
     segment2 = base_height + 1.5
     total_height = segment2 + 2.5
@@ -94,42 +94,44 @@ def l_shaped_x_axis(step_size=0.01):
         x_data.append(x)
         y_data.append(y)
         z_data.append(z)
-        iin += step_size
+        iin += 1
 
     return list(zip(x_data, y_data, z_data))
 
-def l_shaped_y_axis(step_size=0.01):
+def l_shaped_y_axis(angle = 45):
     x_data = []
     y_data = []
     z_data = []
-    total_steps = 20
     base_height = 1
     segment2 = base_height + 1.5
     total_height = segment2 + 2.5
+    slighted_height = 3
     iin = 0
+    z_axix = 0
     base_steps = base_height * (total_steps / total_height)
     
     while iin < total_steps:
         if iin < base_steps:
             radius = 0
+            z_axix = base_height * (iin / base_steps)
         else:
-            radius = radius_of_coil * ( iin - base_steps)/(total_steps - base_steps)
+            radius = slighted_height * ( iin - base_steps)/(total_steps - base_steps) * math.sin(math.radians(angle))
+            z_axix = base_height + slighted_height * ( iin - base_steps)/(total_steps - base_steps) * math.cos(math.radians(angle))
 
         x = round(radius_of_coil, 4)
         y = round(radius + radius_of_coil, 4)
-        z = round(iin * total_height / total_steps, 4)
+        z = round(z_axix, 4)
         x_data.append(x)
         y_data.append(y)
         z_data.append(z)
-        iin += step_size
+        iin += 1
 
     return list(zip(x_data, y_data, z_data))
 
-def l_shaped_xy_axis(step_size=0.01):
+def l_shaped_xy_axis():
     x_data = []
     y_data = []
     z_data = []
-    total_steps = 20
     base_height = 1
     segment2 = base_height + 1.5
     total_height = segment2 + 2.5
@@ -148,6 +150,6 @@ def l_shaped_xy_axis(step_size=0.01):
         x_data.append(x)
         y_data.append(y)
         z_data.append(z)
-        iin += step_size
+        iin += 1
 
     return list(zip(x_data, y_data, z_data))
