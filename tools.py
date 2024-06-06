a2df1611053e65940993db81bc993c03598bea28
Naresh Dhungana <hinareshg@gmail.com>
2024-06-07 00:25:37 +0900

    juh

---
+++ tools.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+def install_package(package_name):
+    try:
+        __import__(package_name)
+        print(f"The package '{package_name}' is already installed.")
+    except ImportError:
+        print(f"The package '{package_name}' is not installed. Installing now...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+        print(f"The package '{package_name}' has been installed successfully.")
+
+# Example usage
+install_package('graphviz')
