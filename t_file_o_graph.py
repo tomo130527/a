7f3270976b7b4af555428c64a7d3460cc42958fd
Naresh Dhungana <hinareshg@gmail.com>
2024-06-14 17:41:15 +0900

    ff

--- t_file_o_graph.py
+++ t_file_o_graph.py
@@ -24,14 +24,29 @@ def plot_csv_data(file):
 
 
 
-def plot_csv_data_two(file1, file2):
+def plot_csv_data_two(file1, file2, include_columns=None):
     # Read the CSV files
     df1 = pd.read_csv(file1, index_col=0)  # Assume the first column is the index
     df2 = pd.read_csv(file2, index_col=0)  # Assume the first column is the index
 
+    # Add suffixes to column names to distinguish between the files
+    df11 = df1.add_suffix('_file1')
+    df22 = df2.add_suffix('_file2')
+
+    
     # Create a new DataFrame to hold concatenated data
-    concatenated_df = pd.concat([df1.add_suffix('_file1'), df2.add_suffix('_file2')], axis=1)
+    concatenated_df = pd.concat([df11, df22], axis=1)
     concatenated_df = concatenated_df.reset_index(drop=True)  # Reset index to get rid of index issues
+
+    # Include only specified columns if any
+    if include_columns is not None:
+        include_columns_file1 = [col + '_file1' for col in include_columns]
+        include_columns_file2 = [col + '_file2' for col in include_columns]
+        include_columns = include_columns_file1 + include_columns_file2
+        # Filter columns that are present in concatenated_df
+        include_columns = [col for col in include_columns if col in concatenated_df.columns]
+        print(f"Including columns: {include_columns}")
+        concatenated_df = concatenated_df[include_columns]
 
     # Plot the data
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -41,17 +56,17 @@ def plot_csv_data_two(file1, file2):
     ax.set_title('Data from both files', fontsize=16)
     ax.set_xlabel('Index', fontsize=12)
     ax.set_ylabel('Values', fontsize=12)
-    ax.legend(title='Columns')
-
+    ax.legend().set_visible(False)
+    
     # Improve the appearance
-    ax.grid(True, linestyle='--', alpha=0.7)
+    ax.grid(True, linestyle='--', alpha=1.0)
     plt.tight_layout()
     plt.show()
 
 
 
-file1 = double_backslashes(r"C:\Users\nares\Desktop\allout\240614\saa.csv")
-file2 = double_backslashes(r"C:\Users\nares\Desktop\allout\240614\thh.csv")
-
+file1 = double_backslashes(r"C:\Users\nares\Desktop\LAB\movie\two\small.csv")
+file2 = double_backslashes(r"C:\Users\nares\Desktop\LAB\movie\two\big.csv")
+exclude_columns = ['SD']
 # Plot both files
-plot_csv_data_two(file1,file2)
+plot_csv_data_two(file1,file2,exclude_columns)
